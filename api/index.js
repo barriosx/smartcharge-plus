20b8fef91207f83d27da328578668bf06e424d6c
barriosx <stevenbarrios03@gmail.com>
2019-04-08 18:05:54 -0400

    First commit

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const CREDS = require('../cred'); // hide these variables
+async function getSmartCharge() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  var responseData = {};
+  // EventEmmiter set up
+  // Intercept xhr request that contain the data I need 
+  page.on('response', response => {
+    if ((response.url().includes('summary')) && (response.status() == 200)) {
+      response.json().then(data => Object.assign(responseData, data))
+      .catch(err => console.log(`Ran into error, here it is: \n${err}\nNext time we will ignore this`))
+    }
+  })
+
+  // Login to dashboard 
+  await page.goto('https://smartchargerewards.fleetcarma.com/login')
+  await page.waitForSelector('button.auth0-lock-submit', { visible: true })
+  await page.type('input[name="email"]', CREDS.username) 
+  await page.type('input[name="password"]', CREDS.password) 
+  await page.click('button.auth0-lock-submit') // Clicking login button
+
+  await page.waitForNavigation({ waitUntil: 'networkidle0'}) // wait until we successfully login
+  
+  // Done
+  await browser.close()
+  return responseData;
+}
+
+module.exports = (router) => {
+  router.get('/', (request,response) => {
+    getSmartCharge().then(data => {
+      response.status(200).json({success: true, data: data});
+    })
+    .catch(err => {
+      response.status(500).json({success: false, message: err });
+    })
+  });
+  return router;
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const server = express();
+const router = express.Router(); // Create router
+const path = require('path'); // Needed for routing
+const smartcharge = require('./api')(router);
+const PORT = process.env.PORT || 3001;
+
+server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/build')));
+server.use('/api', smartcharge);
+
+server.get('/',(req, res) => {
+  // Send frontend
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+
+
+server.listen(PORT, () => console.log('Listening on port ' + PORT))
